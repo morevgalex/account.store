04d089d1cc04f5675e16eade6e2b4bbed6565dc6
Evgeniy <mor.evg.alex@gmail.com>
2020-08-08 22:09:12 +0300

    added models

--- backend/accstore/accstore_app/models.py
+++ backend/accstore/accstore_app/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=128, db_index=True)
+    description = models.TextField(null=True, blank=True)
+
+class Object(models.Model):
+    name = models.CharField(max_length=128, primary_key=True, db_index=True)
+
+class Game_Object(models.Model):
+    pass
+
+class Attribute(models.Model):
+    pass
+
+class Value(models.Model):
+    pass
+
+class Product(models.Model):
+    pass
--- backend/accstore/accstore_app/views.py
+++ backend/accstore/accstore_app/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello, I am game-store.pro!')
