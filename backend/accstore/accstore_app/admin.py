6910c9b77fd7e25b9d23a479045fa24ad383b871
Evgeniy <mor.evg.alex@gmail.com>
2020-08-09 15:45:52 +0300

    added models

--- backend/accstore/accstore_app/admin.py
+++ backend/accstore/accstore_app/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+from .models import *
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('title',)
+
+
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Object, ObjectAdmin)
+admin.site.register(Game_Object)
+admin.site.register(Attribute)
+admin.site.register(Value)
+admin.site.register(Product)
+# admin.site.register(Game)
--- backend/accstore/accstore_app/models.py
+++ backend/accstore/accstore_app/models.py
@@ -2,20 +2,87 @@ from django.db import models
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=128, db_index=True)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(verbose_name='Название', primary_key=True, max_length=128, db_index=True)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
 
 class Object(models.Model):
-    name = models.CharField(max_length=128, primary_key=True, db_index=True)
+    name = models.CharField(verbose_name='Название', max_length=128, primary_key=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Объект'
+        verbose_name_plural = 'Объекты'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Game_Object(models.Model):
-    pass
+    game_title = models.ForeignKey(Game, on_delete=models.PROTECT, verbose_name = 'Игра', db_index=True)
+    object_name = models.ForeignKey(Object, on_delete=models.PROTECT, verbose_name='Объект', db_index=True)
+
+    class Meta:
+        unique_together = (('game_title', 'object_name'),)
+        verbose_name = 'Игра и объект'
+        verbose_name_plural = 'Игры и объекты'
+        ordering = ['game_title', 'object_name']
+
+    def __str__(self):
+        return f'{self.game_title} {self.object_name}'
+
 
 class Attribute(models.Model):
-    pass
+    name = models.CharField(verbose_name='Название', max_length=128, db_index=True)
+    game_object_id = models.ForeignKey(Game_Object, on_delete=models.PROTECT, verbose_name='Объект игры')
+
+    TYPES = (
+        ('Целый', 'Число'),
+        ('Дробный', 'Дробный'),
+        ('Строка', 'Строка'),
+        ('Булево', 'Булево'),
+        ('Дата', 'Дата'),
+    )
+    type_of = models.CharField(max_length=16, choices=TYPES)
+
+    ANSWERS = (
+        (True, 'Да'),
+        (False, 'Нет'),
+    )
+    is_predefined = models.BooleanField(verbose_name='Предопределенный атрибут?', choices=ANSWERS)
+
+    class Meta:
+        verbose_name = 'Аттрибут объекта игры'
+        verbose_name_plural = 'Аттрибуты объекта игры'
+
+    def __str__(self):
+        return f'{self.game_object_id} {self.name} {self.type_of}'
+
 
 class Value(models.Model):
-    pass
+    attribute_id = models.ForeignKey(Attribute, on_delete=models.PROTECT, verbose_name='Аттрибут')
+    string_value = models.CharField(verbose_name='Строковое значение', max_length=64, blank=True, null=True)
+    integer_value = models.IntegerField(verbose_name='Целое значение', blank=True, null=True)
+    float_value = models.FloatField(verbose_name='Дробное значение', blank=True, null=True)
+    bool_value = models.BooleanField(verbose_name='Булево значение', blank=True, null=True)
+    date_value = models.DateField(verbose_name='Дата', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Значение аттрибута объекта игры'
+        verbose_name_plural = 'Значения аттрибута объекта игры'
+
+    def __str__(self):
+        return f'{self.attribute_id} {self.string_value or self.integer_value or self.float_value or self.bool_value or self.date_value}'
+
+
 
 class Product(models.Model):
     pass
