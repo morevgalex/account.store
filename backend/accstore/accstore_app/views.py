22f34393c9171416704f3faffaf1870fc9a3c352
Evgeniy <mor.evg.alex@gmail.com>
2020-08-20 14:47:11 +0300

    optimized filter

--- backend/accstore/accstore_app/views.py
+++ backend/accstore/accstore_app/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.db.models import QuerySet, Q
+from django.db.models import QuerySet, Q, FilteredRelation
 from .models import *
 from .create_models import add
+from django.db.models import Subquery
 
 
 def index(request):
@@ -17,7 +18,7 @@ def index(request):
 
 def game_page(request, game_slug):
     game = Game.objects.get(slug=game_slug)
-    game_objects = Game_Object.objects.filter(game_title=game.title)
+    game_objects = Game_Object.objects.filter(game__title=game.title)
     context = {'game': game, 'game_objects': game_objects}
     return render(request, 'accstore_app/game_page.html', context)
 
@@ -44,26 +45,16 @@ def add_product_page(request, game_slug, object_slug):
 
 
 def filter_page(request):
-    game_object = Game_Object.objects.get(game__slug='wow', object__slug='account')
-    # product_values1 = Product_Value.objects.filter(product__game_object=game_object, value__value='Dwarf')
-    # print(product_values1)
-    # product_values2 = Product_Value.objects.filter(product__game_object=game_object, value__value='3')
-    # print(product_values2)
+    print('----------------------')
+    game_object = Game_Object.objects.select_related('game', 'object').get(game__slug='wow', object__slug='account')
 
-    # почему это работает? ведь после intersection получается пустой QuerySet,
-    # но values_list показывает что сущности, имеющие product в нем есть
 
-    # products_id = product_values1.intersection(product_values2).values_list('product')
-    # print(products_id)
-    # products = Product.objects.filter(pk__in=list(map(lambda x: x[0],products_id)))
-    # print(products)
-    condition = Q(product__game_object=game_object) & (Q(value__attribute__name='Race', value__value='Dwarf') | Q(value__attribute__name='Level', value__value='3'))
-    products_id_list: QuerySet = list(map(lambda x: x[0], Product_Value.objects.filter(condition).values_list('product')))
-    print(f'products_id_list: {products_id_list}')
-    # 2 - число атрибутов объекта игры, по которым происходит сортировка, в данном случае 2 атрибута: раса и уровень
-    products_id_list_filtered = list(filter(lambda x: products_id_list.count(x) == 2, products_id_list))
-    print(f'products_id_list_filtered: {products_id_list_filtered}')
-    products = Product.objects.filter(pk__in=products_id_list_filtered)
+    condition0 = Q(game_object=game_object)
+    condition1 = Q(value__attribute__name='Race', value__value='Dwarf')
+    condition2 = Q(value__attribute__name='Level', value__value='3')
+    products = Product.objects.filter(condition0, pk__in=Product_Value.objects.filter(condition1).values('product').filter(product__in=Subquery(Product_Value.objects.filter(condition2).values('product'))))
+
+
     context = {'game_object': game_object, 'products': products}
     return render(request, 'accstore_app/filter_page.html', context)
 
